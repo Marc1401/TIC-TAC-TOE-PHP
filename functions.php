711b47f3f9ad4a10695812b7a5405a13d246ccce
Marc1401 <virgilekouame14@gmail.com>
2023-03-19 11:50:16 -0400

    correction 1

--- functions.php
+++ functions.php
@@ -1,26 +1,20 @@
 <?php 
-session_start();
 
+session_start();
 // Report simple running errors
 error_reporting(E_ERROR| E_PARSE);
 
 
 // Commencons par enregistrer les joueurs
 
-function registerPlayers($playerX="", $playerY="",)
+function registerPlayers($playerX="", $playerO="")
 {
-    $_SESSION['PLAYYER_X_NAME'] = $playerX;
-    $_SESSION['PLAYYER_O_NAME'] = $playerO;
+    $_SESSION['PLAYER_X_NAME'] = $playerX;
+    $_SESSION['PLAYER_O_NAME'] = $playerO;
     setTurn('x');
     resetBoard();
     resetWins();
 } 
-
-function setTurn($turn='x') 
-{
-    $_SESSION['TURN'] = $turn;
-}
-
 function resetBoard() 
 {
     $_SESSION['PLAYS'] = 0;
@@ -29,39 +23,12 @@ function resetBoard()
         unset($_SESSION['CELL_'. $i]);
     }
 }
+
 function resetWins() 
 {
     $_SESSION['PLAYER_X_WINS'] = 0;
     $_SESSION['PLAYER_O_WINS'] = 0;
 }
-
-// Une fois enrgistrer ajoutons les fonctions pour jouer 
-
-//Affichage du resultat
-function playersRegistered() 
-{
-    return $_SESSION['PLAYER_X_NAME'] && $_SESSION['PLAYER_O_NAME'];
-}
-
-
-function playerName($player='x') {
-    return $_SESSION['PLAYER' . strtoupper($player) . '_NAME'];
-}
-
-function score($player='x') 
-{
-    $score = $_SESSION['PLAYER_' . strtoupper($player) . '_WINS'];
-    return $score ? $score : 0;
-}
-
-
-/*
-
-
-
-
-
-*/
 
 function playsCount() {
     return $_SESSION['PLAYS'] ? $_SESSION['PLAYS'] : 0;
@@ -77,9 +44,35 @@ function addPlaysCount() {
 
 
 
+
+function playerName($player='x') {
+    return $_SESSION['PLAYER_' . strtoupper($player) . '_NAME'];
+}
+
+
+
+// Une fois enrgistrer ajoutons les fonctions pour jouer 
+
+//Affichage du resultat
+function playersRegistered() 
+{
+    return $_SESSION['PLAYER_X_NAME'] && $_SESSION['PLAYER_O_NAME'];
+}
+
+function setTurn($turn='x') 
+{
+    $_SESSION['TURN'] = $turn;
+}
+
+
+
 function getTurn() {
     return $_SESSION['TURN'] ? $_SESSION['TURN'] : 'x';
 }
+
+
+
+
 
 function markWin($player='x') {
     $_SESSION['PLAYER_' . strtoupper($player) . '_WINS']++;
@@ -165,7 +158,11 @@ function isDiagonalWin($turn='x') {
     return $win && getCell(5) == $turn;
 }
 
-
+function score($player='x') 
+{
+    $score = $_SESSION['PLAYER_' . strtoupper($player) . '_WINS'];
+    return $score ? $score : 0;
+}
 
 
 ?>
